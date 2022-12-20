a309653d52e70ecacdc5922f46853a21bafb331d
SPehsar <cloud_data_sec_mgmt@yahoo.com>
2022-12-20 10:42:19 -0500

    some recent needed changess to fix issues

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 
 export const AddTransaction = () => {
-  const [text, setText] = useState("");
-  const [amount, setAmount] = useState(0);
+  const [text, setText] = useState({null});
+  
   return (
     <>
       <h3>Add new transaction</h3>
@@ -10,14 +10,14 @@ export const AddTransaction = () => {
         
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
+          <input type="text" value={text} onChange={(e) => setText(err.target.value)} placeholder="Enter text..." />
         </div>
         <div className="form-control">
           <label htmlFor="amount">
             Amount <br />
             (negative - expense, positive - income)
           </label>
-          <input type="number"  value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
+          <input type="number"  value={amount} onChange={(e) => setAmount(err.target.value)} placeholder="Enter amount..." />
         </div>
         <div>
           <button className="btn">Add transaction</button>
--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -3,10 +3,10 @@ import React from 'react'
 export const IncomeExpenses = () => {
   return (
     
-    <div className="inc-exp-container">
+    <div className="inc-exp-containers">
         <div>
             <h4>Income</h4>
-            <p  className="money plus">-$0.0</p>
+            <p  className="money pius">-$0.0</p>
         </div>
         <div>
             <h4>Expense</h4>
