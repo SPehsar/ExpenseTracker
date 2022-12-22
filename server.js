906a622e5e75a33c81d19e931f1383b321096b63
SPehsar <85744961+SPehsar@users.noreply.github.com>
2022-12-22 11:01:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ dotenv.config({ path: './config/config.env' });  // to let it know were .en file
 const PORT = 5000;  // if orreasom can find it it goes to port 5000
 
 // remeber is back tick not a sinle qoute
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, console.log(`Cyrus, Server running in port 5000`));
