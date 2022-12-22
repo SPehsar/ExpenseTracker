5982492c133a735671678709cef154c34b25cdd0
SPehsar <85744961+SPehsar@users.noreply.github.com>
2022-12-22 11:47:30 -0500

    Update server.js
    moving environmental variables to where they belong. (.env)  Created config folder and put the .env file type there and modified sever.js

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 
-const app = express(); 
-dotenv.config({ path: './config/config.env' });  // to let it know were .en file is
 
-const PORT = 5000;  // if orreasom can find it it goes to port 5000
+dotenv.config({ path: './config/config.env' });  // to let it know were .env file is
+
+const app = express();
+
+app.get('/', (req, res) => res.send('hello world'))
+const PORT = process.env.PORT || 5000;  // if for some reasom cannot find it it goes to port 5000
 
 // remeber is back tick not a sinle qoute
-app.listen(PORT, console.log(`Cyrus, Server running in port 5000`));
+app.listen(PORT, console.log(`Cyrus, Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
