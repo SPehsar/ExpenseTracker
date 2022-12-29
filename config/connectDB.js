153d3312110fd1cff608118a5c5283c68c11b470
SPehsar <cloud_data_sec_mgmt@yahoo.com>
2022-12-29 08:00:33 -0500

    some codes and flders clean up

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
-const colors = require("colors");
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log(`Server Running On ${mongoose.connection.host}`.bgCyan.white);
+    console.log(`Server Running On ${mongoose.connection.host}`);
   } catch (error) {
-    console.log(`${error}`.bgYellow);
+    console.log(`${error}`);
   }
 };
 
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require("express")
 const cors = require("cors")
-const morgan = require("morgan")
 const dotenv = require("dotenv")
-const colors = require("colors")
 const connectDB = require("./config/connectDB")
+
 // config dot env
 dotenv.config()
 
@@ -14,7 +13,6 @@ connectDB()
 const app=express()
 
 // middleware
-app.use(morgan("dev"))
 app.use(express.json())
 app.use(cors())
 
@@ -28,5 +26,5 @@ const PORT = 8080 || process.env.PORT
 
 // listen server
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`.bgWhite.blue)
+    console.log(`Server running on port ${PORT}`)
 })
